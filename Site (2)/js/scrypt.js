d8d375f0f543ce2acb8df74c668524d526c6d803
TTXSTM <71548215+TTXSTM@users.noreply.github.com>
2022-11-17 01:28:42 +0300

    Fix zoom Pt. 1
    Removing zoom, and redesigning

--- Site (2)/js/scrypt.js
+++ Site (2)/js/scrypt.js
@@ -1,3 +1,11 @@
+const header = document.querySelector('.head_scroll');
+document.addEventListener('DOMContentLoader', () => {
+    window.addEventListener('scroll', () => {
+        let scrollTop = window.scrollY;
+        console.log(scrollTop);
+    });
+});
+
 function myFunction(){
     document.getElementById('paragraph').innerHTML ='Недостаточно средств';
     alert('Недостаточно средств');
