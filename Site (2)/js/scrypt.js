32e12c5e32e1d710866d1aca0f81e68e9f411cd2
TTXSTM <71548215+TTXSTM@users.noreply.github.com>
2022-11-16 23:05:32 +0300

    Js add
    Added js to html

---
+++ Site (2)/js/scrypt.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    document.getElementById('paragraph').innerHTML ='Недостаточно средств';
+    alert('Недостаточно средств');
+}
+
+function noSell(){
+    alert('У вас нет доступа к этой функции')
+}
